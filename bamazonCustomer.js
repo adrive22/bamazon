7e1bc1b340de08651fe6caecac1e73785b0637d5
Allison Driver <allisondriver@Allisons-MacBook-Pro.local>
2018-04-19 16:07:59 -0500

    added sql queries for product input info

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,7 +13,7 @@ connection.connect(function(err){
 		console.log(err);
 	}
 	console.log("connected as id" + connection.threadId);
-	connection.end();
+	
 });
 
 function showProducts() {
@@ -28,20 +28,51 @@ function showProducts() {
 showProducts();
 
  
-function processAnswers(answers){
-	console.log("Answers: ", answers)
+inquirer.prompt([
 
-	var questions = [
 {
-	message: "What's the id number of the item you would like to buy?",
-	type: "input",
-	name: "idNumber"
-}];
+	type:"input",
+	name:"whatProduct",
+	message:"What is the id number of the product you would like to buy?"
+},{
+	type:"input",
+	name:"howMany",
+	message:"How many units would you like to buy?"
+}
 
-inquirer.prompt(questions,processAnswers);
-};
- 
-processAnswers();
+]).then(function(inquirerResponse){
+	var inputID = inquirerResponse.whatProduct
+	var inputQuantity = inquirerResponse.howMany
+	console.log(inputID);
+	console.log(inputQuantity);
+
+function checkQuantity(){
+	connection.query("SELECT * FROM products WHERE item_id=?", [inputID], 
+		function(err, res){
+			if (err)
+				return console.log(err);
+	
+			console.log(res);
+		
+			for (let i = 0; i < res.length; i++){
+				var product = res[i];
+				console.log(product);
+				console.log(product.product_name)
+			}
+		}
+	)
+}
+
+		
+
+
+
+checkQuantity();
+
+});
+
+
+
 
 
 
